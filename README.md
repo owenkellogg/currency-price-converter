b2844403de9d7487a5d8528ab4a94399754331f4
Steven Zeiler <me@stevenzeiler.com>
2018-06-16 10:39:24 -0400

    Command line tool to calculate prices

---
+++ README.md
@@ -0,0 +1,12 @@
+# Currency Price Converter
+
+## Convert 1.5 XAU (ounces gold) to ETH
+
+```
+./program.js calculate XAU 1.5 ETH
+
+1 BTC = 5.011386 XAU
+1 BTC = 13.052018821011139 ETH
+1.5 XAU = 7.813418575825812 ETH
+```
+
---
+++ program.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const program = require('commander');
+const winston = require('winston');
+const lib = require('./lib');
+
+const log = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console()
+  ]
+})
+
+program
+  .command('calculate <base_currency> <amount> <target_currency>')
+  .action(async (base, amount, target) => {
+    let prices = await lib.getAllPrices();
+
+    console.log(`1 BTC = ${prices[base]} ${base}`)
+    console.log(`1 BTC = ${prices[target]} ${target}`)
+
+    let targetAmount = await lib.convert({ currency: "XAU", value: 3 }, 'ETH');
+
+    console.log(`${amount} ${base} = ${targetAmount} ${target}`);
+  });
+
+program.parse(process.argv);
+
